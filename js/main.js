62bc106a438a710a4d41b25bd740cd2e4a9167bc
lucasdiaz <lucasssdiazz96@gmail.com>
2023-03-01 21:41:33 -0300

    2da entega java script lucas diaz

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-var MiNombre=prompt("Escriba su nombre");
+let MiNombre=prompt("Escriba su nombre");
         console.log("El usuario dijo que su nombre es")
         if (!isNaN(MiNombre)){
             alert("No debe poner numeros en este campo")
             console.error("El usuario puso un numero donde va su nombre: " + MiNombre)
         }
-        var MiApellido=prompt("Escriba su apellido");
+        let MiApellido=prompt("Escriba su apellido")
         console.log("El usuario dijo que su apellido es: "+MiApellido)
         if (!isNaN(MiApellido)){
             alert("No debe poner numeros en este campo")
             console.error("El usuario puso un numero donde va su nombre")
         }
-        var MiAntiguedad=parseFloat(prompt("Escriba su antiguedad en la empresa"))
+        const MiAntiguedad=parseFloat(prompt("Escriba su antiguedad en la empresa"))
         console.log("El usuario dijo que su antiguedad es " + MiAntiguedad)
         if (isNaN(MiAntiguedad)){
             alert("Debe poner numeros en este campo")
             console.error("El usuario puso un valor invalido")
         }
-        var MisDias=parseInt(prompt("Escriba la cantidad de dias que trabaja en el mes"))
-        console.log("El usuario dijo que la cantidad de dias que trabajo en el mes son " + MisDias)
+        const MisDias=parseInt(prompt("Escriba la cantidad de dias que trabaja en el mes"))
+        console.log("El usuario dijo que la cantidad de dias que trabajo en el mes son " + MisDias);
 
         if (MisDias > 31){
             alert("La cantidad de dias que trabajo en el mes no puede ser mayor a 31")
@@ -36,15 +36,19 @@ var MiNombre=prompt("Escriba su nombre");
             console.info("el usuario no califica para el bono de presentismo")
         }
         
-        
+        const valores=[80000,110000,150000];
+        console.log(valores);
+        for (let i = 0; i < 3; i++){
+            console.log(valores[i]);
+        }
         // la antiguedad dandole al usuario la informacion de cuanto es el valor de su sueldo segun la antiguedad que tenga
-        var ValorAntiguedad;
+        let ValorAntiguedad;
         
-        (MiAntiguedad < 3 && MiAntiguedad >= 0)? ValorAntiguedad = 80000 : (MiAntiguedad >= 3 && MiAntiguedad < 5)? ValorAntiguedad = 110000 :(MiAntiguedad>= 5)? ValorAntiguedad = 150000 : (alert("El valor es invalido por favor ingresar la cantidad de años en numero"),console.error("El valor no corresponde"))
+        (MiAntiguedad < 3 && MiAntiguedad >= 0)? ValorAntiguedad = 80000 : (MiAntiguedad >= 3 && MiAntiguedad < 5)? ValorAntiguedad = 110000 :(MiAntiguedad>= 5)? ValorAntiguedad = 150000 : (alert("El valor es invalido por favor ingresar la cantidad de años en numero"))
         
         //declaramos una variable para dar la informacion si corresponde el bono por presentismo segun la cantidad de dias informados
-        var BonoPresentismo=((MisDias>=20 && MisDias < 32) ? BonoPresentismo = 10000 :0)
+        var BonoPresentismo=((MisDias>= 20 && MisDias < 32) ? BonoPresentismo = 10000 :0)
         //aclaramos cual es el sueldo total para poder informar al usuario
-        var SueldoTotal= (ValorAntiguedad + BonoPresentismo )
+        const SueldoTotal= (ValorAntiguedad + BonoPresentismo )
         //y por ultimo damos toda la informacion solicitada
         document.write("Hola "+ MiNombre + " " + MiApellido + " con sus " + MiAntiguedad + " años de antiguedad a un valor mensual de " + ValorAntiguedad  + "\n Usted cuenta con un  bono de presentismo " + "\n Lo que significa que su sueldo final es: $" + SueldoTotal)
